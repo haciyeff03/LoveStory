8dcabc6796f477adac4e7654ac4ebc991fb1fddb
karamsafarli <karamslee007@gmail.com>
2023-07-27 03:38:09 +0400

    mobile navbar

--- client/components/layout/Navbar/Navbar.jsx
+++ client/components/layout/Navbar/Navbar.jsx
@@ -1,6 +1,8 @@
 import { Link } from 'react-router-dom';
 import './navbar.scss';
+import { useState } from 'react';
 const Navbar = () => {
+  const [toggleMenu, setToggleMenu] = useState(false);
   return (
     <header>
       <div className="overlay"></div>
@@ -20,7 +22,7 @@ const Navbar = () => {
             </Link>
           </div>
 
-          <div className="burger_menu">
+          <div className="burger_menu" onClick={() => setToggleMenu(true)}>
             <svg xmlns="http://www.w3.org/2000/svg" width="32" height="23" viewBox="0 0 32 23" fill="none">
               <path d="M2.28577 2.28564H29.7143M2.28577 11.4285H29.7143M2.28577 20.5714H29.7143" stroke="white" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
             </svg>
@@ -35,10 +37,23 @@ const Navbar = () => {
         </div>
       </div>
 
-      <div className="mobile_menu">
-        
+      <div className="mobile_menu" style={{ transform: toggleMenu ? 'translateY(0)' : 'translateY(-100%)' }}>
+        <div className="close-btn">
+          <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 32 32" fill="none" onClick={() => setToggleMenu(false)}>
+            <path d="M1 16C1 19.2712 1.17658 21.8316 1.62331 23.8417C2.06751 25.8404 2.7654 27.2332 3.76611 28.2339C4.76681 29.2346 6.15959 29.9325 8.15832 30.3767C10.1684 30.8234 12.7288 31 16 31C19.2712 31 21.8316 30.8234 23.8417 30.3767C25.8404 29.9325 27.2332 29.2346 28.2339 28.2339C29.2346 27.2332 29.9325 25.8404 30.3767 23.8417C30.8234 21.8316 31 19.2712 31 16C31 12.7288 30.8234 10.1684 30.3767 8.15832C29.9325 6.15959 29.2346 4.76681 28.2339 3.76611C27.2332 2.7654 25.8404 2.06751 23.8417 1.62331C21.8316 1.17658 19.2712 1 16 1C12.7288 1 10.1684 1.17658 8.15832 1.62331C6.15959 2.06751 4.76681 2.7654 3.76611 3.76611C2.7654 4.76681 2.06751 6.15959 1.62331 8.15832C1.17658 10.1684 1 12.7288 1 16Z" stroke="#222222" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
+            <path d="M20 12L16 16M16 16L12 20M16 16L20 20M16 16L12 12" stroke="#222222" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
+          </svg>
+        </div>
+        <div className="menu_links">
+          <Link to='/'>
+            Giriş
+          </Link>
+          <Link to='/'>
+            Qeydiyyat
+          </Link>
+        </div>
       </div>
-    </header>
+    </header >
   )
 }
 
