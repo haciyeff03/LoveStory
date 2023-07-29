001ad36eacac844f0397f35fffcba07262da2ac5
alihasanli <ahasanli639.dev@gmail.com>
2023-07-29 16:47:49 +0400

    Creating loading animation (Loading.jsx and loading.css)

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,6 +2,7 @@ import { Routes, Route, useLocation } from "react-router-dom";
 import Layout from "../components/layout/layout";
 import ScrollToTop from "../components/ScrollToTop";
 import Error from "../components/pages/Error/Error";
+import Loading from "../components/pages/Loading/Loading";
 import Login from '../components/pages/Login/Login';
 import Signup from '../components/pages/Signup/Signup';
 import Home from '../components/pages/Home/Home';
@@ -10,7 +11,7 @@ import '../styles/global.css';
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 import '../node_modules/bootstrap/dist/js/bootstrap.bundle.js';
 const App = () => {
-  const pathnameArr = ['/', '/login', '/signup', '/profile'];
+  const pathnameArr = ['/', '/login', '/signup', '/profile', '/loading'];
   const location = useLocation();
 
 
@@ -19,10 +20,11 @@ const App = () => {
       <ScrollToTop />
       {
         pathnameArr.includes(location.pathname) ? (
-          location.pathname === "/login" || location.pathname === "/signup" || location.pathname === '/error' ? (
+          location.pathname === "/login" || location.pathname === "/signup" || location.pathname === '/error' || location.pathname === '/loading' ? (
             <Routes>
               <Route element={<Login />} path="/login" />
               <Route element={<Signup />} path="/signup" />
+              <Route element={<Loading />} path="/loading" />
             </Routes>
           ) : (
 
