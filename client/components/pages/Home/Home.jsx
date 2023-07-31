2f559117a4ba93d8b193b472ec3a25cb55e8a6a2
karamsafarli <karamslee007@gmail.com>
2023-07-31 04:33:01 +0400

    Homepage Card Component is ready

--- client/components/pages/Home/Home.jsx
+++ client/components/pages/Home/Home.jsx
@@ -1,7 +1,50 @@
+import CardComponent from "../../FeedCards/CardComponent"
+import './home.scss'
 
 const Home = () => {
   return (
-    <div>Home</div>
+    <div className="home_page">
+      <div className="container">
+        <div className="row g-4">
+          <div className="col-lg-4">
+            <CardComponent />
+          </div>
+          <div className="col-lg-4">
+            <CardComponent />
+          </div>
+          <div className="col-lg-4">
+            <CardComponent />
+          </div>
+          <div className="col-lg-4">
+            <CardComponent />
+          </div>
+          <div className="col-lg-4">
+            <CardComponent />
+          </div>
+          <div className="col-lg-4">
+            <CardComponent />
+          </div>
+          <div className="col-lg-4">
+            <CardComponent />
+          </div>
+          <div className="col-lg-4">
+            <CardComponent />
+          </div>
+          <div className="col-lg-4">
+            <CardComponent />
+          </div>
+          <div className="col-lg-4">
+            <CardComponent />
+          </div>
+          <div className="col-lg-4">
+            <CardComponent />
+          </div>
+          <div className="col-lg-4">
+            <CardComponent />
+          </div>
+        </div>
+      </div>
+    </div>
   )
 }
 
